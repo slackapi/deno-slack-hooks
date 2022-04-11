a0558a0c88e296ce4395f237554a1c4ebda2c429
Sarah Jiang <sarahjiang@slack-corp.com>
2022-04-11 16:51:15 -0700

    restructure

--- src/mod.ts
+++ src/mod.ts
@@ -3,24 +3,11 @@ export const RUNTIME_TAG = "deno_slack_runtime@0.0.3";
 
 export const projectScripts = () => {
   return {
-    "get-manifest": {
-      "script": {
-        "default":
-          `deno run -q --unstable --import-map=import_map.json --allow-read --allow-net https://deno.land/x/${BUILDER_TAG}/mod.ts --manifest`,
-      },
-    },
-    "build": {
-      "script": {
-        "default":
-          `deno run -q --unstable --import-map=import_map.json --allow-read --allow-write --allow-net https://deno.land/x/${BUILDER_TAG}/mod.ts`,
-      },
-    },
-    "start": {
-      "script": {
-        "default":
-          `deno run -q --unstable --import-map=import_map.json --allow-read --allow-net https://deno.land/x/${RUNTIME_TAG}/mod.ts`,
-      },
-      "watcher": {
+    "get-manifest": `deno run -q --unstable --import-map=import_map.json --allow-read --allow-net https://deno.land/x/${BUILDER_TAG}/mod.ts --manifest`,
+    "build": `deno run -q --unstable --import-map=import_map.json --allow-read --allow-write --allow-net https://deno.land/x/${BUILDER_TAG}/mod.ts`,
+    "start": `deno run -q --unstable --import-map=import_map.json --allow-read --allow-net https://deno.land/x/${RUNTIME_TAG}/mod.ts`,
+    "config": {
+      "watch": {
         "filter_regex": "^manifest\\.(ts|js|json)$",
         "paths": ["."],
       },
