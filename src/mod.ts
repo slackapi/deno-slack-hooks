01c73044bd33e10e29019569c499531915989358
Sarah Jiang <sarahjiang@slack-corp.com>
2022-04-11 17:08:31 -0700

    Fix formatting

--- src/mod.ts
+++ src/mod.ts
@@ -4,9 +4,12 @@ export const RUNTIME_TAG = "deno_slack_runtime@0.0.3";
 export const projectScripts = () => {
   return {
     "hooks": {
-      "get-manifest": `deno run -q --unstable --import-map=import_map.json --allow-read --allow-net https://deno.land/x/${BUILDER_TAG}/mod.ts --manifest`,
-      "build": `deno run -q --unstable --import-map=import_map.json --allow-read --allow-write --allow-net https://deno.land/x/${BUILDER_TAG}/mod.ts`,
-      "start": `deno run -q --unstable --import-map=import_map.json --allow-read --allow-net https://deno.land/x/${RUNTIME_TAG}/local-run.ts`,
+      "get-manifest":
+        `deno run -q --unstable --import-map=import_map.json --allow-read --allow-net https://deno.land/x/${BUILDER_TAG}/mod.ts --manifest`,
+      "build":
+        `deno run -q --unstable --import-map=import_map.json --allow-read --allow-write --allow-net https://deno.land/x/${BUILDER_TAG}/mod.ts`,
+      "start":
+        `deno run -q --unstable --import-map=import_map.json --allow-read --allow-net https://deno.land/x/${RUNTIME_TAG}/local-run.ts`,
     },
     "config": {
       "watch": {
