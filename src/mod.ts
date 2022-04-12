013df27a7aa8a28788dea4ee87b83f9b724455cf
Filip Maj <fmaj@slack-corp.com>
2022-04-12 15:55:42 -0400

    Bump runtime to 0.0.4

--- src/mod.ts
+++ src/mod.ts
@@ -1,5 +1,5 @@
 export const BUILDER_TAG = "deno_slack_builder@0.0.7";
-export const RUNTIME_TAG = "deno_slack_runtime@0.0.3";
+export const RUNTIME_TAG = "deno_slack_runtime@0.0.4";
 
 export const projectScripts = () => {
   return {
