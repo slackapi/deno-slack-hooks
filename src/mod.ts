ba3b6f4df8921d0c616f7ed37c83055523499dfc
Neil Shapiro <nshapiro@slack-corp.com>
2022-04-28 15:36:44 -0700

    Bumping builder to 0.0.10

--- src/mod.ts
+++ src/mod.ts
@@ -1,4 +1,4 @@
-export const BUILDER_TAG = "deno_slack_builder@0.0.9";
+export const BUILDER_TAG = "deno_slack_builder@0.0.10";
 export const RUNTIME_TAG = "deno_slack_runtime@0.0.6";
 
 export const projectScripts = () => {
