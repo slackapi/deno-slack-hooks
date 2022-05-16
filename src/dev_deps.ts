729d03d7a20b2b460ad3410b3a322bcee45ac3dc
Maurice Codik Moscoso <mcodik@slack-corp.com>
2022-05-16 14:17:44 -0400

    Remove unused deps

--- src/dev_deps.ts
+++ src/dev_deps.ts
@@ -1,6 +1,1 @@
-export {
-  assertEquals,
-  assertRejects,
-  assertStringIncludes,
-} from "https://deno.land/std@0.132.0/testing/asserts.ts";
-export * from "https://deno.land/x/mock_fetch@0.3.0/mod.ts";
+export { assertEquals } from "https://deno.land/std@0.132.0/testing/asserts.ts";
