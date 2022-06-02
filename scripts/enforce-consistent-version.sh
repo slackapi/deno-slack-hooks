df9b3a4193907e4144a908cd0c2058f8e3c77c48
Filip Maj <fmaj@slack-corp.com>
2022-06-02 17:41:54 -0400

    Extract the tag from the library source before changing branches

--- scripts/enforce-consistent-version.sh
+++ scripts/enforce-consistent-version.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 set -e
+# Extract the version for deno-slack-hooks embedded in src/libraries.ts
+LIBRARY_TAG=$(grep "\[DENO_SLACK_HOOKS\]:" src/libraries.ts | awk '{print $2}' | cut -c2- | rev | cut -c3- | rev);
 # Intended to be used in GitHub CI, so lots of the basic and core branches need to be explicitly pulled in
 git fetch origin main;
 git checkout main;
 git pull --tags origin main;
 # Grab the latest git tag available on the origin remote
 GIT_TAG=$(git describe --tags --abbrev=0);
-# Extract the version for deno-slack-hooks embedded in src/libraries.ts
-LIBRARY_TAG=$(grep "\[DENO_SLACK_HOOKS\]:" src/libraries.ts | awk '{print $2}' | cut -c2- | rev | cut -c3- | rev);
 
 if [ "$GIT_TAG" = "$LIBRARY_TAG" ]; then
   echo "Versions consistent; we good.";
