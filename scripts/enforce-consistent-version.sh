e39ec9a59164b4861ae037992765b46d02193fda
Filip Maj <fmaj@slack-corp.com>
2022-06-02 17:47:05 -0400

    Vastly simplify logic, no more futzing with branches and origins and remotes

--- scripts/enforce-consistent-version.sh
+++ scripts/enforce-consistent-version.sh
@@ -2,12 +2,8 @@
 set -e
 # Extract the version for deno-slack-hooks embedded in src/libraries.ts
 LIBRARY_TAG=$(grep "\[DENO_SLACK_HOOKS\]:" src/libraries.ts | awk '{print $2}' | cut -c2- | rev | cut -c3- | rev);
-# Intended to be used in GitHub CI, so lots of the basic and core branches need to be explicitly pulled in
-git fetch origin main;
-git checkout main;
-git pull --tags origin main;
 # Grab the latest git tag available on the origin remote
-GIT_TAG=$(git describe --tags --abbrev=0);
+GIT_TAG=$(git -c 'versionsort.suffix=-' ls-remote --exit-code --refs --sort='version:refname' --tags https://github.com/slackapi/deno-slack-hooks '*.*.*' | tail --lines=1 | cut -d '/' -f 3);
 
 if [ "$GIT_TAG" = "$LIBRARY_TAG" ]; then
   echo "Versions consistent; we good.";
