9bea58c51c37c1b8bbea6f557c977c7805310863
Neil Shapiro <nshapiro@slack-corp.com>
2022-07-13 17:07:00 -0700

    Adding create-trigger hook

---
+++ src/create-trigger.ts
@@ -0,0 +1,52 @@
+import { parse, path } from "./deps.ts";
+
+const createTrigger = async () => {
+  const source = parse(Deno.args).source as string;
+
+  if (!source) throw new Error("A source path needs to be defined");
+
+  const fullPath = path.isAbsolute(source)
+    ? source
+    : path.join(Deno.cwd(), source || "");
+
+  return await readFile(fullPath);
+};
+
+const readFile = async (path: string) => {
+  try {
+    const { isFile } = await Deno.stat(path);
+    if (!isFile) throw new Error("The specified source is not a valid file.");
+    if (path.endsWith(".json")) return readJSONFile(path);
+    return readTSFile(path);
+  } catch (e) {
+    if (e instanceof Deno.errors.NotFound) {
+      throw new Error("Trigger Definition file cannot be found");
+    }
+    throw e;
+  }
+};
+
+const readJSONFile = async (path: string) => {
+  try {
+    const jsonString = await Deno.readTextFile(path);
+    return JSON.parse(jsonString);
+  } catch (e) {
+    throw e;
+  }
+};
+
+const readTSFile = async (path: string) => {
+  try {
+    const file = await import(`file://${path}`);
+    if (file && !file.default) {
+      throw new Error("The Trigger Definition isn't being exported by default");
+    }
+    return file;
+  } catch (e) {
+    throw e;
+  }
+};
+
+if (import.meta.main) {
+  console.log(JSON.stringify(await createTrigger()));
+}
--- src/deps.ts
+++ src/deps.ts
@@ -1,1 +1,2 @@
 export { parse } from "https://deno.land/std@0.138.0/flags/mod.ts";
+export * as path from "https://deno.land/std@0.134.0/path/mod.ts";
