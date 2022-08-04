0a2685997bb3c9e9f96fe41d48830186aa2aa9c7
Alissa Renz <alissa.renz@gmail.com>
2022-08-04 10:53:56 -0700

    Remove note and return of empty array if error thrown

--- src/utilities.ts
+++ src/utilities.ts
@@ -10,12 +10,6 @@ export async function getJSON(file: string): Promise<JSONValue> {
     const fileContents = await Deno.readTextFile(file);
     return parse(fileContents);
   } catch (err) {
-    // If supported dependency file was not found, return silent failure.
-    // The reason for this is that there may be any combination of supported
-    // dependency files used. The only case where bubbling this up would apply
-    // is if *no* dependency files were found, which, since slack.json is both
-    // necessary for the project AND a dependency file, is not a valid use case.
-    if (err instanceof Deno.errors.NotFound) return {};
     throw new Error(err.message, { cause: err });
   }
 }
