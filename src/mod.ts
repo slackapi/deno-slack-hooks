260e72aa442e448e06d7b456db291766b9ca836a
Filip Maj <fmaj@slack-corp.com>
2023-02-10 11:48:47 -0500

    Pull in split out protocol repo, use protocol in get-trigger hook implementation, have get-hooks payload self-reference using import.meta.url instead of a hard-coded tag, point builder URL to specific PoC github branch/commit SHA.

--- src/mod.ts
+++ src/mod.ts
@@ -7,17 +7,17 @@ export const projectScripts = (args: string[]) => {
     "runtime": "deno",
     "hooks": {
       "get-manifest":
-        `deno run -q --config=deno.jsonc --allow-read --allow-net --allow-env file:///Users/fmaj/src/deno-slack-builder/src/mod.ts --manifest`,
+        `deno run -q --config=deno.jsonc --allow-read --allow-net --allow-env https://raw.githubusercontent.com/slackapi/deno-slack-builder/79a47b0588c2325a4ca2f9fd74cce65074564644/src/mod.ts --manifest`,
       "get-trigger":
-        `deno run -q --config=deno.jsonc --allow-read --allow-net --allow-env https://deno.land/x/${HOOKS_TAG}/get_trigger.ts`,
+        `deno run -q --config=deno.jsonc --allow-read --allow-net --allow-env ${import.meta.url}/../get_trigger.ts`,
       "build":
-        `deno run -q --config=deno.jsonc --allow-read --allow-write --allow-net --allow-run --allow-env https://deno.land/x/${BUILDER_TAG}/mod.ts`,
+        `deno run -q --config=deno.jsonc --allow-read --allow-write --allow-net --allow-run --allow-env https://raw.githubusercontent.com/slackapi/deno-slack-builder/79a47b0588c2325a4ca2f9fd74cce65074564644/src/mod.ts`,
       "start":
         `deno run -q --config=deno.jsonc --allow-read --allow-net --allow-run --allow-env https://deno.land/x/${RUNTIME_TAG}/local-run.ts ${startHookFlags}`,
       "check-update":
-        `deno run -q --config=deno.jsonc --allow-read --allow-net https://deno.land/x/${HOOKS_TAG}/check_update.ts`,
+        `deno run -q --config=deno.jsonc --allow-read --allow-net ${import.meta.url}/../check_update.ts`,
       "install-update":
-        `deno run -q --config=deno.jsonc --allow-run --allow-read --allow-write --allow-net https://deno.land/x/${HOOKS_TAG}/install_update.ts`,
+        `deno run -q --config=deno.jsonc --allow-run --allow-read --allow-write --allow-net ${import.meta.url}/../install_update.ts`,
     },
     "config": {
       "protocol-version": ["message-boundaries", "dont-cross-the-streams"],
