dd827f9d37e8888b0a0c0a3bc168e71e5d674197
Filip Maj <fmaj@slack-corp.com>
2023-02-10 14:07:34 -0500

    use protocol POC branch for runtime

--- src/deps.ts
+++ src/deps.ts
@@ -1,5 +1,5 @@
 export { parse } from "https://deno.land/std@0.138.0/flags/mod.ts";
 export * as path from "https://deno.land/std@0.134.0/path/mod.ts";
 export type { JSONValue } from "https://deno.land/std@0.149.0/encoding/jsonc.ts";
-export { getProtocolInterface } from "https://raw.githubusercontent.com/filmaj/deno-slack-protocols/0396e7d/src/mod.ts";
-export type { Protocol } from "https://raw.githubusercontent.com/filmaj/deno-slack-protocols/0396e7d/src/mod.ts";
+export { getProtocolInterface } from "https://raw.githubusercontent.com/filmaj/deno-slack-protocols/831a5f8/src/mod.ts";
+export type { Protocol } from "https://raw.githubusercontent.com/filmaj/deno-slack-protocols/831a5f8/src/mod.ts";
