d906957afc3bc865973072dc736fe3c664cf656e
Filip Maj <fmaj@slack-corp.com>
2023-03-03 09:22:44 -0500

    TODOs around updating protocol dependency URLs once ready.

--- src/deps.ts
+++ src/deps.ts
@@ -3,5 +3,6 @@ export * as path from "https://deno.land/std@0.134.0/path/mod.ts";
 export { ensureDir } from "https://deno.land/std@0.134.0/fs/mod.ts";
 export type { JSONValue } from "https://deno.land/std@0.149.0/encoding/jsonc.ts";
 export { deepMerge } from "https://deno.land/std@0.134.0/collections/deep_merge.ts";
+// TODO: update the URLs here once the protocols repo passes review and has a release
 export { getProtocolInterface } from "https://raw.githubusercontent.com/slackapi/deno-slack-protocols/initial/src/mod.ts";
 export type { Protocol } from "https://raw.githubusercontent.com/slackapi/deno-slack-protocols/initial/src/types.ts";
--- src/dev_deps.ts
+++ src/dev_deps.ts
@@ -11,6 +11,6 @@ export {
 } from "https://deno.land/std@0.177.0/testing/mock.ts";
 export type { Spy } from "https://deno.land/std@0.177.0/testing/mock.ts";
 export * as mockFetch from "https://deno.land/x/mock_fetch@0.3.0/mod.ts";
-
 export * as mockFile from "https://deno.land/x/mock_file@v1.0.1/mod.ts";
+// TODO: update the URLs here once the protocols repo passes review and has a release
 export { MockProtocol } from "https://raw.githubusercontent.com/slackapi/deno-slack-protocols/initial/src/mock.ts";
