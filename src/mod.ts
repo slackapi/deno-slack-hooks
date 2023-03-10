6b1411e12282f582c431cb52be02a872874eb66e
Filip Maj <fmaj@slack-corp.com>
2023-03-10 14:39:17 -0800

    Remove use of import.meta.url from this PR for now.

--- src/mod.ts
+++ src/mod.ts
@@ -1,5 +1,6 @@
-// TODO: once deno-slack-runtime is released with new protocol support, update this URL and the RUNTIME_TAG appropriately.
-// import { RUNTIME_TAG } from "./libraries.ts";
+// TODO: once deno-slack-runtime is released with new protocol support, import and use the RUNTIME_TAG appropriately.
+// TODO: as per below TODOs also reuse the appropriate HOOKS tag in the URLs below prior to merge/release
+// import { HOOKS_TAG } from "./libraries.ts";
 import { getStartHookAdditionalDenoFlags } from "./flags.ts";
 export { getProtocolInterface } from "./deps.ts";
 export type { Protocol } from "./deps.ts";
@@ -10,18 +11,23 @@ export const projectScripts = (args: string[]) => {
     "runtime": "deno",
     "hooks": {
       "get-manifest":
-        `deno run -q --config=deno.jsonc --allow-read --allow-net --allow-env ${import.meta.url}/../get_manifest.ts`,
+        // TODO: temporarily reference this branch when loading hook code for itself (this makes it easier to test). prior to merging, need to update the URL here to reference the expected hooks version this change will be released under.
+        `deno run -q --config=deno.jsonc --allow-read --allow-net --allow-env https://raw.githubusercontent.com/slackapi/deno-slack-hooks/multi-protocols-with-builder/src/get_manifest.ts`,
       "get-trigger":
-        `deno run -q --config=deno.jsonc --allow-read --allow-net --allow-env ${import.meta.url}/../get_trigger.ts`,
+        // TODO: temporarily reference this branch when loading hook code for itself (this makes it easier to test). prior to merging, need to update the URL here to reference the expected hooks version this change will be released under.
+        `deno run -q --config=deno.jsonc --allow-read --allow-net --allow-env https://raw.githubusercontent.com/slackapi/deno-slack-hooks/multi-protocols-with-builder/src/get_trigger.ts`,
       "build":
-        `deno run -q --config=deno.jsonc --allow-read --allow-write --allow-net --allow-run --allow-env ${import.meta.url}/../build.ts`,
+        // TODO: temporarily reference this branch when loading hook code for itself (this makes it easier to test). prior to merging, need to update the URL here to reference the expected hooks version this change will be released under.
+        `deno run -q --config=deno.jsonc --allow-read --allow-write --allow-net --allow-run --allow-env https://raw.githubusercontent.com/slackapi/deno-slack-hooks/multi-protocols-with-builder/src/build.ts`,
       "start":
         // TODO: once deno-slack-runtime is released with new protocol support, update this URL and the RUNTIME_TAG appropriately.
         `deno run -q --config=deno.jsonc --allow-read --allow-net --allow-run --allow-env https://raw.githubusercontent.com/slackapi/deno-slack-runtime/protocol-support/src/local-run.ts ${startHookFlags}`,
       "check-update":
-        `deno run -q --config=deno.jsonc --allow-read --allow-net ${import.meta.url}/../check_update.ts`,
+        // TODO: temporarily reference this branch when loading hook code for itself (this makes it easier to test). prior to merging, need to update the URL here to reference the expected hooks version this change will be released under.
+        `deno run -q --config=deno.jsonc --allow-read --allow-net https://raw.githubusercontent.com/slackapi/deno-slack-hooks/multi-protocols-with-builder/src/check_update.ts`,
       "install-update":
-        `deno run -q --config=deno.jsonc --allow-run --allow-read --allow-write --allow-net ${import.meta.url}/../install_update.ts`,
+        // TODO: temporarily reference this branch when loading hook code for itself (this makes it easier to test). prior to merging, need to update the URL here to reference the expected hooks version this change will be released under.
+        `deno run -q --config=deno.jsonc --allow-run --allow-read --allow-write --allow-net https://raw.githubusercontent.com/slackapi/deno-slack-hooks/multi-protocols-with-builder/src/install_update.ts`,
     },
     "config": {
       "protocol-version": ["message-boundaries"],
