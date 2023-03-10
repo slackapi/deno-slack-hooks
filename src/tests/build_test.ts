cdef78f3aa3ba6497c1ac1e321cb0bc45d1b50eb
Filip Maj <fmaj@slack-corp.com>
2023-03-10 14:54:01 -0800

    Add a test to ensure functions of type=API are not bundled in the build hook.

--- src/tests/build_test.ts
+++ src/tests/build_test.ts
@@ -13,6 +13,7 @@ Deno.test("build hook tests", async (t) => {
     await tt.step("should exist", () => {
       assertExists(validateAndCreateFunctions);
     });
+
     await tt.step(
       "should not throw an exception when fed a function file that has a default export",
       async () => {
@@ -47,6 +48,7 @@ Deno.test("build hook tests", async (t) => {
         assertSpyCalls(logSpy, 2);
       },
     );
+
     await tt.step(
       "should throw an exception when fed a function file that does not have a default export",
       async () => {
@@ -83,6 +85,7 @@ Deno.test("build hook tests", async (t) => {
         );
       },
     );
+
     await tt.step(
       "should throw an exception when fed a function file that has a non-function default export",
       async () => {
@@ -119,5 +122,39 @@ Deno.test("build hook tests", async (t) => {
         );
       },
     );
+
+    await tt.step("should not write out functions of type 'API'", async () => {
+      const hookCLI = MockProtocol();
+      const manifest = {
+        "functions": {
+          "test_function": {
+            "title": "Test function",
+            "description": "this is a test",
+            "source_file":
+              "src/tests/fixtures/functions/test_function_not_function_file.ts",
+            "type": "API",
+            "input_parameters": {
+              "required": [],
+              "properties": {},
+            },
+            "output_parameters": {
+              "required": [],
+              "properties": {},
+            },
+          },
+        },
+      };
+      const outputDir = await Deno.makeTempDir();
+      await validateAndCreateFunctions(
+        Deno.cwd(),
+        outputDir,
+        manifest,
+        hookCLI,
+      );
+      const logSpy = hookCLI.log as Spy;
+      // Assert that we log out that we removed the output directory - but not that we wrote out a function file - and that we only had a single log call.
+      assertStringIncludes(logSpy.calls[0].args[0], "removed directory");
+      assertSpyCalls(logSpy, 1);
+    });
   });
 });
