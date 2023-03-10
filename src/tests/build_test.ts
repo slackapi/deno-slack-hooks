5e5fe7ff5aa2a6f666b692e470a66752a26deffe
Filip Maj <fmaj@slack-corp.com>
2023-03-10 14:59:42 -0800

    Add a few more build hook tests.

--- src/tests/build_test.ts
+++ src/tests/build_test.ts
@@ -123,6 +123,78 @@ Deno.test("build hook tests", async (t) => {
       },
     );
 
+    await tt.step(
+      "should throw an exception when manifest entry for a function points to a non-existent file",
+      async () => {
+        const hookCLI = MockProtocol();
+        const manifest = {
+          "functions": {
+            "test_function": {
+              "title": "Test function",
+              "description": "this is a test",
+              "source_file":
+                "src/tests/fixtures/functions/test_function_this_file_does_not_exist.ts",
+              "input_parameters": {
+                "required": [],
+                "properties": {},
+              },
+              "output_parameters": {
+                "required": [],
+                "properties": {},
+              },
+            },
+          },
+        };
+        const outputDir = await Deno.makeTempDir();
+        await assertRejects(
+          () =>
+            validateAndCreateFunctions(
+              Deno.cwd(),
+              outputDir,
+              manifest,
+              hookCLI,
+            ),
+          Error,
+          "Could not find file",
+        );
+      },
+    );
+
+    await tt.step(
+      "should throw an exception when manifest entry for a function does not have a source_file defined",
+      async () => {
+        const hookCLI = MockProtocol();
+        const manifest = {
+          "functions": {
+            "test_function": {
+              "title": "Test function",
+              "description": "this is a test",
+              "input_parameters": {
+                "required": [],
+                "properties": {},
+              },
+              "output_parameters": {
+                "required": [],
+                "properties": {},
+              },
+            },
+          },
+        };
+        const outputDir = await Deno.makeTempDir();
+        await assertRejects(
+          () =>
+            validateAndCreateFunctions(
+              Deno.cwd(),
+              outputDir,
+              manifest,
+              hookCLI,
+            ),
+          Error,
+          "no source_file was provided",
+        );
+      },
+    );
+
     await tt.step("should not write out functions of type 'API'", async () => {
       const hookCLI = MockProtocol();
       const manifest = {
