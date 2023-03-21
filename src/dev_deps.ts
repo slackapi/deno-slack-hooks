37896bf37b873ce9b4369194958f91078ccc227b
Filip Maj <fmaj@slack-corp.com>
2023-03-21 13:19:53 -0400

    Point deno-slack-protocols dev dependency to released version.

--- src/dev_deps.ts
+++ src/dev_deps.ts
@@ -14,5 +14,4 @@ export {
 export type { Spy } from "https://deno.land/std@0.177.0/testing/mock.ts";
 export * as mockFetch from "https://deno.land/x/mock_fetch@0.3.0/mod.ts";
 export * as mockFile from "https://deno.land/x/mock_file@v1.0.1/mod.ts";
-// TODO: update the URLs here once the protocols repo passes review and has a release
-export { MockProtocol } from "https://raw.githubusercontent.com/slackapi/deno-slack-protocols/initial/src/mock.ts";
+export { MockProtocol } from "https://deno.land/x/deno_slack_protocols@0.0.2/mock.ts";
