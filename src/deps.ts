f16802bd2bc417316388dd9019cbd6f533489771
Filip Maj <fmaj@slack-corp.com>
2023-03-21 12:27:41 -0400

    Point deno-slack-protocols dependency to released version.

--- src/deps.ts
+++ src/deps.ts
@@ -3,6 +3,5 @@ export * as path from "https://deno.land/std@0.134.0/path/mod.ts";
 export { ensureDir } from "https://deno.land/std@0.134.0/fs/mod.ts";
 export type { JSONValue } from "https://deno.land/std@0.149.0/encoding/jsonc.ts";
 export { deepMerge } from "https://deno.land/std@0.134.0/collections/deep_merge.ts";
-// TODO: update the URLs here once the protocols repo passes review and has a release
-export { getProtocolInterface } from "https://raw.githubusercontent.com/slackapi/deno-slack-protocols/initial/src/mod.ts";
-export type { Protocol } from "https://raw.githubusercontent.com/slackapi/deno-slack-protocols/initial/src/types.ts";
+export { getProtocolInterface } from "https://deno.land/x/deno_slack_protocols@0.0.2/mod.ts";
+export type { Protocol } from "https://deno.land/x/deno_slack_protocols@0.0.2/types.ts";
