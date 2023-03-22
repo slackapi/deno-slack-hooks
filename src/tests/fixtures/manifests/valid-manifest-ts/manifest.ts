cab05708ff50d7b861a3b09503f7bf467a40abe5
Filip Maj <fmaj@slack-corp.com>
2023-03-22 13:53:09 -0400

    Dont add dependency to deno-slack-sdk in manifest test fixture file, just mimic structure of manifest object should be sufficient.

--- src/tests/fixtures/manifests/valid-manifest-ts/manifest.ts
+++ src/tests/fixtures/manifests/valid-manifest-ts/manifest.ts
@@ -1,8 +1,6 @@
-import { Manifest } from "https://deno.land/x/deno_slack_sdk/mod.ts";
-
-export default Manifest({
-  name: "surreptitious-nardwhal-420",
-  description: "a valid minimal manifest.ts for testing purposes",
-  icon: "lol.png",
-  botScopes: ["chat.write"]
-});
+export default {
+  display_information: {
+    name: "surreptitious-nardwhal-420",
+    description: "a valid minimal manifest.ts for testing purposes",
+  }
+};
