74f27588f33bf1911d77e2637b0db7eff5ab8b24
Filip Maj <fmaj@slack-corp.com>
2023-03-23 16:35:59 -0400

    Revert mod.ts temporary testing changes.

--- src/mod.ts
+++ src/mod.ts
@@ -1,5 +1,4 @@
-// TODO: as per below TODOs also reuse the appropriate HOOKS tag in the URLs below prior to merge/release
-import { /*HOOKS_TAG*/ RUNTIME_TAG } from "./libraries.ts";
+import { HOOKS_TAG, RUNTIME_TAG } from "./libraries.ts";
 import { getStartHookAdditionalDenoFlags } from "./flags.ts";
 
 export const projectScripts = (args: string[]) => {
@@ -8,22 +7,17 @@ export const projectScripts = (args: string[]) => {
     "runtime": "deno",
     "hooks": {
       "get-manifest":
-        // TODO: temporarily use -r and reference this branch when loading hook code for itself (this makes it easier to test). prior to merging, need to update the URL here to reference the expected hooks version this change will be released under by using the HOOKS_TAG import at the top.
-        `deno run -r -q --config=deno.jsonc --allow-read --allow-net --allow-env https://raw.githubusercontent.com/slackapi/deno-slack-hooks/multi-protocols-with-builder/src/get_manifest.ts`,
+        `deno run -q --config=deno.jsonc --allow-read --allow-net --allow-env https://deno.land/x/${HOOKS_TAG}/get_manifest.ts`,
       "get-trigger":
-        // TODO: temporarily use -r and reference this branch when loading hook code for itself (this makes it easier to test). prior to merging, need to update the URL here to reference the expected hooks version this change will be released under by using the HOOKS_TAG import at the top.
-        `deno run -r -q --config=deno.jsonc --allow-read --allow-net --allow-env https://raw.githubusercontent.com/slackapi/deno-slack-hooks/multi-protocols-with-builder/src/get_trigger.ts`,
+        `deno run -q --config=deno.jsonc --allow-read --allow-net --allow-env https://deno.land/x/${HOOKS_TAG}/get_trigger.ts`,
       "build":
-        // TODO: temporarily use -r and reference this branch when loading hook code for itself (this makes it easier to test). prior to merging, need to update the URL here to reference the expected hooks version this change will be released under by using the HOOKS_TAG import at the top.
-        `deno run -r -q --config=deno.jsonc --allow-read --allow-write --allow-net --allow-run --allow-env https://raw.githubusercontent.com/slackapi/deno-slack-hooks/multi-protocols-with-builder/src/build.ts`,
+        `deno run -q --config=deno.jsonc --allow-read --allow-write --allow-net --allow-run --allow-env https://deno.land/x/${HOOKS_TAG}/build.ts`,
       "start":
         `deno run -q --config=deno.jsonc --allow-read --allow-net --allow-run --allow-env https://deno.land/x/${RUNTIME_TAG}/local-run.ts ${startHookFlags}`,
       "check-update":
-        // TODO: temporarily use -r and reference this branch when loading hook code for itself (this makes it easier to test). prior to merging, need to update the URL here to reference the expected hooks version this change will be released under by using the HOOKS_TAG import at the top.
-        `deno run -r -q --config=deno.jsonc --allow-read --allow-net https://raw.githubusercontent.com/slackapi/deno-slack-hooks/multi-protocols-with-builder/src/check_update.ts`,
+        `deno run -q --config=deno.jsonc --allow-read --allow-net https://deno.land/x/${HOOKS_TAG}/check_update.ts`,
       "install-update":
-        // TODO: temporarily use -r and reference this branch when loading hook code for itself (this makes it easier to test). prior to merging, need to update the URL here to reference the expected hooks version this change will be released under by using the HOOKS_TAG import at the top.
-        `deno run -r -q --config=deno.jsonc --allow-run --allow-read --allow-write --allow-net https://raw.githubusercontent.com/slackapi/deno-slack-hooks/multi-protocols-with-builder/src/install_update.ts`,
+        `deno run -q --config=deno.jsonc --allow-run --allow-read --allow-write --allow-net https://deno.land/x/${HOOKS_TAG}/install_update.ts`,
     },
     "config": {
       "protocol-version": ["message-boundaries"],
