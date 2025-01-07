1e63b748e1a577c682ad1403d1e9ba3f75584c2b
William Bergamin <wbergamin@salesforce.com>
2025-01-07 15:41:30 +0000

    chore: update the esbuild dependencies (#98)

--- src/bundler/esbuild_bundler.ts
+++ src/bundler/esbuild_bundler.ts
@@ -1,5 +1,5 @@
-import * as esbuild from "npm:esbuild@0.23.1";
-import { denoPlugins } from "jsr:@luca/esbuild-deno-loader@0.10.3";
+import * as esbuild from "npm:esbuild@0.24.2";
+import { denoPlugins } from "jsr:@luca/esbuild-deno-loader@0.11.1";
 
 type EsbuildBundleOptions = {
   /** The path to the file being bundled */
